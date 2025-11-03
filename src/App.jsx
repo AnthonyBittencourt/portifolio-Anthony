0dff53524a3d0d60f1527e071875422eeb4190ac
ANTHNO <anthonyppbittencourt@gmail.com>
2025-11-03 11:45:17 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styles from'./App.module.css'
+import fotoperfil from '../public/foto1.png'
 
 function App() {
 
@@ -11,19 +12,25 @@ function App() {
         <a href="#s4">Curiosidade</a>
       </nav>
       <main>
-        <section>
-          <h2>sessao 1</h2>
+        <section className={styles.s1} id="s1">
+          <div className={styles.left}>
+            <img className={styles.imgPerfil} src={fotoperfil} alt=''/>
+          </div>
+          <div className={styles.right}>
+            <h2 className={styles.title}>Anthony Bittencourt</h2>
+            <p className={styles.paragraph}>Desenvolvedor intermediario, com habilidades em linguagens C, Python, JavaScript, CSS e HTML. Atualmente cursando Tecnico em Analise e Desenvolvimento de Sistemas</p>
+          </div>
         </section>
 
-        <section>
+        <section id="s2">
           <h2>sessao 2</h2>
         </section>
 
-        <section>
+        <section id="s3">
           <h2>sessao 3</h2>
         </section>
 
-        <section>
+        <section id="s4">
           <h2>sessao 4</h2>
         </section>
       </main>
